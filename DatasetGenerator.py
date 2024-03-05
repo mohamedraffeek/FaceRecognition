92421a825e56794e59130b39796764e80a6a4398
MohAmin22 <alsayedm12@gmail.com>
2024-03-05 08:33:06 +0200

    Hyper_parameter tuning has been finished

--- DatasetGenerator.py
+++ DatasetGenerator.py
@@ -13,10 +13,10 @@ mainDirectory = "/home/mohamed/CSED_25/Year_3/term_2/PR-ML/my_labs/lab_1/faces"
 def generateData():
     # Iterate the sub-directories, and load each set of images as 10 (number of images for each subject) 
     # 1-d vectors (using np.ravel) into the data matrix
-    for subDirectory in os.listdir(mainDirectory):
+    for subDirectory in sorted(os.listdir(mainDirectory)):
         path = os.path.join(mainDirectory, subDirectory)
         if os.path.isdir(path):
-            for image in os.listdir(path):
+            for image in sorted(os.listdir(path)):
                 dataMatrix.append(np.ravel(iio.imread(os.path.join(path, image))))
                 # Fill up the label vector --> We can concatanate this to the data matrix if we needed
                 labelVector.append(int(subDirectory[1:3]))
