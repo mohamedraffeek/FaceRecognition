609102da078987491a6c09ee9ae8dd7abc24c805
Mohamed <mohamedraffeek2001@gmail.com>
2024-03-05 07:42:22 +0200

    Finished the faces vs non faces lda part

--- main.py
+++ main.py
@@ -3,6 +3,6 @@
 import DatasetSplitter as ds
 from LDA import LDA
 
-trainingData, trainingLabels, testingData, testingLabels = ds.splitData()
+trainingData, trainingLabels, testingData, testingLabels = ds.splitData2()
 
 LDA(trainingData, trainingLabels, testingData, testingLabels)
