a05441aec83da2a0eeb4f4d7272ee67f0ca0162f
MohAmin22 <alsayedm12@gmail.com>
2024-03-05 20:05:49 +0200

    charts

--- DatasetSplitter.py
+++ DatasetSplitter.py
@@ -17,3 +17,19 @@ def splitData():
             testingData.append(dataMatrix[i])
             testingLabels.append(labelVector[i])
     return trainingData, trainingLabels, testingData, testingLabels
+
+def differentSplitData():
+    trainingData = []
+    testingData = []
+    trainingLabels = []
+    testingLabels = []
+    for i in range(0, len(dataMatrix), 10): # Jump by 10 each iteration
+        for j in range(10):
+            ind = i + j
+            if ind < 7:
+                trainingData.append(dataMatrix[ind])
+                trainingLabels.append(labelVector[ind])
+            else:
+                testingData.append(dataMatrix[ind])
+                testingLabels.append(labelVector[ind])
+    return trainingData, trainingLabels, testingData, testingLabels
